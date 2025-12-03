eedbc1fbd721f1c3ecc51fd086c5fd1c3e94d9d1
Farhan Jamil <farhanjamil1666@gmail.com>
2025-12-03 12:47:06 +0500

    Add files via upload

---
+++ first.py
@@ -0,0 +1,9 @@
+# Solution-1  (Using Exponentation)
+num = int(input("Enter a number a here:"))
+sr = num**(1/2)
+print("The square root of the given number is",sr)
+# Solution-2  (Using Math Module)
+import math
+num1 = int(input("Enter a number here:"))
+sr1 = math.sqrt(num1)
+print("The square root of the given number is",sr1)
